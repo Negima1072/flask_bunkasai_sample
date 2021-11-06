f6ce2a721e1e7a7124d717c8204fa7364b1b7643
Negima1072 <tkser@icloud.com>
2021-11-06 23:03:55 +0900

    init

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("./templates/index.html", static_folder="./static")
+    return render_template("templates/index.html", static_folder="./static")
 
 if __name__ == '__main__':
     app.run()
