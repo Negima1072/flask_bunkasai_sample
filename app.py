cb1ed2753e53aae4c3fa2391c68e374b39ce6dcd
Negima1072 <tkser@icloud.com>
2021-11-06 23:06:32 +0900

    init

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='html/static', template_folder='html/templates')
 
 @app.route('/')
 def index():
-    return render_template("templates/index.html", static_folder="./static")
+    return render_template("index.html", static_folder="./static")
 
 if __name__ == '__main__':
     app.run()
